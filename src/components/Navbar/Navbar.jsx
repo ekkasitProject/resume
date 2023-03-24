a044cbe93f4ac41e2582446c68953e150504e325
ekkasit ponkhunthod <ekkasitprivate@gmail.com>
2023-03-24 21:08:06 +0700

    add navber burger-menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import classes from './Navbar.module.css';
+import { CgMenuRightAlt } from 'react-icons/cg';
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className={classes.navbar}>
       <h3 className={classes.logo}>Ekkasit.dev</h3>
-      <ul>
+
+      <button
+        className={`${classes.burger} ${isOpen ? classes.open : ''}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <CgMenuRightAlt
+          className={`${classes.menuIcon} ${isOpen ? classes.open : ''}`}
+        />
+      </button>
+
+      <ul className={`${classes.sidebar} ${isOpen ? classes.active : ''}`}>
         <li>
           <Link
             spy={true}
@@ -13,6 +27,7 @@ const Navbar = () => {
             offset={-100}
             duration={1000}
             to="hero"
+            onClick={() => setIsOpen(false)}
           >
             Home
           </Link>
@@ -24,6 +39,7 @@ const Navbar = () => {
             offset={-100}
             duration={1000}
             to="about"
+            onClick={() => setIsOpen(false)}
           >
             About
           </Link>
@@ -35,6 +51,7 @@ const Navbar = () => {
             offset={-100}
             duration={1000}
             to="project"
+            onClick={() => setIsOpen(false)}
           >
             Projects
           </Link>
@@ -46,6 +63,7 @@ const Navbar = () => {
             offset={50}
             duration={1000}
             to="contact"
+            onClick={() => setIsOpen(false)}
           >
             Contact
           </Link>
